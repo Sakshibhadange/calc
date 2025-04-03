ea29d8eb8e4a40962065ad9439911ce961bb7318
unknown <you@example.com>
2025-04-03 15:13:40 +0530

    calculation.java added

---
+++ calculation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner 
+ public class numbers 
+{
+	pubic static void main(String[] args)
+	{
+		Scanner sc= new Scanner(SYstem.in)
+			System.out.println("Enter two numbers");
+		       int num1= sc.nextInt();
+		       int num2= sc.nextInt();
+		    int add = num1 + num2;  
+		    System.out.println("Addition of two numbers is "+add);
+		    sc.close();
+	}
+}
